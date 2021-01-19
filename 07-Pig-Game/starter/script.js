03ccfd04f515a03caf60e080e712dda1febeef89
Magda Barthofer <magda.barthofer@gmail.com>
2021-01-19 14:20:48 +0100

    added 9th challenge

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,19 +1,52 @@
 'use strict';
 let btnNew = document.querySelector('.btn--new');
 let btnRoll = document.querySelector('.btn--roll');
-let btnHold = document.querySelector('.btn--hold');
+const btnHold = document.querySelector('.btn--hold');
+let currentScore0 = document.querySelector('.current--0');
+let currentScore1 = document.querySelector('.current--1');
+const player0 = document.querySelector('.player--0')
+const player1 = document.querySelector('.player--1')
+let activePlayer = 0
+
 let score0 = document.getElementById('score--0');
 let score1 = document.getElementById('score--1');
-const player1 = document.querySelector('.player--1')
-const player2 = document.querySelector('.player--2')
-let playerActive = document.querySelector('.player--active')
+score0 = 0
+score1 = 0
+currentScore0 = 0
+currentScore1 = 0
+
+const switchPlayer = function () {
+    player1.classList.toggle('player--active')
+    player0.classList.toggle('player--active')
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+}
+
 
 
 btnRoll.addEventListener('click', function () {
     let dice = Math.trunc(Math.random() * 6) + 1;
-    console.log(dice)
-    document.querySelector('.dice').src = `dice-${dice}.png`;
-    if 
+    if (dice === 1) {
+        switchPlayer()
+        document.querySelector('.dice').src = `dice-${dice}.png`;
+    }
+    else {
+        document.querySelector('.dice').src = `dice-${dice}.png`;
+        currentScore0 += dice
+        document.getElementById(`current--${activePlayer}`).innerText = currentScore0
+    }
 })
 
 
+btnHold.addEventListener('click', function () {
+   if (activePlayer === 0 ) {
+    score0 += currentScore0
+    document.getElementById('score--0').innerText = score0
+    switchPlayer();
+   }
+   else {
+    score1 += currentScore1
+    document.getElementById('score--1').innerText = score1
+    switchPlayer();
+   }
+})
---
+++ 09-data_structures/script.js
@@ -0,0 +1,56 @@
+// We're building a football betting app (soccer for my American friends 😅)!
+// Suppose we get data from a web service about a certain game ('game' variable on next page). In this challenge we're gonna work with that data.
+// Your tasks:
+// 1. Create one player array for each team(variables'players1'and 'players2')
+// 2. Thefirstplayerinanyplayerarrayisthegoalkeeperandtheothersarefield players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+// 3. Createanarray'allPlayers'containingallplayersofbothteams(22 players)
+// 4. Duringthegame,BayernMunich(team1)used3substituteplayers.Socreatea new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+// 5. Basedonthegame.oddsobject,createonevariableforeachodd(called 'team1', 'draw' and 'team2')
+// 6. Writeafunction('printGoals')thatreceivesanarbitrarynumberofplayer names (not an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+// 7. Theteamwiththeloweroddismorelikelytowin.Printtotheconsolewhich team is more likely to win, without using an if/else statement or the ternary operator.
+// Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+// GOOD LUCK 😀
+
+const game = {
+    team1: 'Bayern Munich', team2: 'Borrussia Dortmund', players: [
+    [
+    'Neuer',
+          'Pavard',
+          'Martinez',
+          'Alaba',
+          'Davies',
+          'Kimmich',
+          'Goretzka',
+          'Coman',
+          'Muller',
+          'Gnarby',
+          'Lewandowski',
+    ], [
+          'Burki',
+          'Schulz',
+          'Hummels',
+          'Akanji',
+          'Hakimi',
+          'Weigl',
+          'Witsel',
+          'Hazard',
+          'Brandt',
+          'Sancho',
+          'Gotze',
+    ], ],
+      score: '4:0',
+      scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+      'Hummels'],
+      date: 'Nov 9th, 2037',
+      odds: {
+    team1: 1.33, x: 3.25, team2: 6.5,
+    }, };
+
+
+    const player1 = [game.players[0]];
+    const player2 = [game.players[1]];
+    const { gk, ...fieldPlayers } = game.player
+    console.log()
+
+    console.log(player1);
+    console.log(player2);
